a2cb54ec49ff8a0741cce4c2b27901c251c606f7
apolopino <apolo.pino@gmail.com>
2021-08-10 03:06:09 +0000

    tests

--- src/models.py
+++ src/models.py
@@ -7,22 +7,23 @@ class People(db.Model):
 	__tablename__ = 'people'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), unique=True, nullable=False)
-	planet = db.relationship('Planets', lazy=True)
+	planet = db.relationship('Planets')
+	planet_name = db.Column(db.String(250), db.ForeignKey('planets.name'))
 
 
 	def serialize(self):
 		return {
 			"id": self.id,
 			"name": self.name,
-			"planet": self.planet
+			"planet": self.planet_name
 		}
 
 class Planets(db.Model):
 	__tablename__ = 'planets'
 	id = db.Column(db.Integer, primary_key=True)
 	name= db.Column(db.String(120), unique=True, nullable=False)
-	people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
-	people = relationship(People)
+	people = db.relationship('People')
+	# inhabitants = db.Column(db.String(250), db.ForeignKey('people.name'))
 
 	def __repr__(self):
 		return self.name
@@ -31,7 +32,7 @@ class Planets(db.Model):
 		return {
 			"id": self.id,
 			"name": self.name,
-			"people_id": self.people_id
+			"people": self.people
 		}
 
 class User(db.Model):
